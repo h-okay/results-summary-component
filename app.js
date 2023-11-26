561f1a30c17121cd57e0066513206872ebc70877
Hakan Okay <hokay18@gmail.com>
2023-11-25 22:14:57 -0500

    Completed the component

---
+++ app.js
@@ -0,0 +1,63 @@
+function generateStat(type, category, score, icon) {
+  return `<div class="stat ${type}">
+    <div class="stat-info">
+      <img
+        class="stat-icon"
+        src="${icon}"
+        alt="${type}-icon"
+      />
+      <p class="stat-name-${type}">${category}</p>
+    </div>
+    <div class="stat-ratings">
+      <p class="stat-score">${score}</p>
+      <span>/</span>
+      <p class="stat-max">100</p>
+    </div>
+  </div>`;
+}
+
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const stats = data.map((stat) =>
+      generateStat(
+        stat.category.toLowerCase(),
+        stat.category,
+        stat.score,
+        stat.icon
+      )
+    );
+    document.querySelector(".stats").innerHTML = stats.join("");
+  });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const resultScoreNumber = document.querySelector(".result-score-number");
+  const achievedScore = 76;
+
+  const interval = 10;
+  const step = achievedScore / (2000 / interval);
+
+  let currentCount = 0;
+
+  function updateScore() {
+    resultScoreNumber.textContent = Math.round(currentCount);
+
+    if (currentCount >= achievedScore) {
+      clearInterval(counterInterval);
+    }
+
+    if (achievedScore - currentCount > 20) {
+      currentCount += step;
+    } else if (achievedScore - currentCount > 10) {
+      currentCount += step / 2;
+    } else if (achievedScore - currentCount > 5) {
+      currentCount += step / 4;
+    } else if (achievedScore - currentCount > 2) {
+      currentCount += step / 8;
+    } else {
+      currentCount += step / 16;
+    }
+  }
+
+  const counterInterval = setInterval(updateScore, interval);
+});
