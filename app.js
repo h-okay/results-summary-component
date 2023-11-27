d6eea7d0f83587f8177e84f2fc6caf475bd497f0
Hakan Okay <hokay18@gmail.com>
2023-11-26 21:44:11 -0500

    Update README

--- app.js
+++ app.js
@@ -16,6 +16,38 @@ function generateStat(type, category, score, icon) {
   </div>`;
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+  const resultScoreNumber = document.querySelector(".result-score-number");
+
+  const achievedScore = 76;
+  const interval = 10;
+  const step = achievedScore / (2000 / interval);
+
+  let currentCount = 0;
+
+  function updateScore() {
+    resultScoreNumber.textContent = Math.round(currentCount);
+
+    if (currentCount >= achievedScore) {
+      clearInterval(counterInterval);
+    }
+    const difference = achievedScore - currentCount;
+    if (difference > 20) {
+      currentCount += step;
+    } else if (difference > 10) {
+      currentCount += step / 2;
+    } else if (difference > 5) {
+      currentCount += step / 4;
+    } else if (difference > 2) {
+      currentCount += step / 8;
+    } else {
+      currentCount += step / 16;
+    }
+  }
+
+  const counterInterval = setInterval(updateScore, interval);
+});
+
 fetch("./data.json")
   .then((response) => response.json())
   .then((data) => {
@@ -29,35 +61,3 @@ fetch("./data.json")
     );
     document.querySelector(".stats").innerHTML = stats.join("");
   });
-
-document.addEventListener("DOMContentLoaded", function () {
-  const resultScoreNumber = document.querySelector(".result-score-number");
-  const achievedScore = 76;
-
-  const interval = 10;
-  const step = achievedScore / (2000 / interval);
-
-  let currentCount = 0;
-
-  function updateScore() {
-    resultScoreNumber.textContent = Math.round(currentCount);
-
-    if (currentCount >= achievedScore) {
-      clearInterval(counterInterval);
-    }
-
-    if (achievedScore - currentCount > 20) {
-      currentCount += step;
-    } else if (achievedScore - currentCount > 10) {
-      currentCount += step / 2;
-    } else if (achievedScore - currentCount > 5) {
-      currentCount += step / 4;
-    } else if (achievedScore - currentCount > 2) {
-      currentCount += step / 8;
-    } else {
-      currentCount += step / 16;
-    }
-  }
-
-  const counterInterval = setInterval(updateScore, interval);
-});
